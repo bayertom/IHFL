628ae543b126cc4c7c65fb9860c634a43bccae64
bayertom <bayertom@natur.cuni.cz>
2022-07-23 15:16:24 +0200

    Data

--- facility.h
+++ facility.h
@@ -3,9 +3,9 @@
 
 //Facility definition
 struct Facility {
-	int u_id;				//Facility center
+	int u_id;				//Facility center, ID of point
 	double fc;				//Facility cost, non-uniform clusterization
-	TVector <int> Cl_id;			//Connected clients
+	TVector <int> Cl_id;			//Connected clients, ID of points
 	bool del;				//Facility marked as deleted
 
 	Facility(const unsigned int& u_id_, const double& fc_) : u_id(u_id_), fc(fc_), del(false) {}
